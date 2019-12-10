e865828e8a1293fd952157e63fe15a56c9d167eb
Fernando Matzdorf <fernando@rindecuentas.org>
2019-12-10 11:59:54 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const es = require('event-stream');
+const JSONStream = require('JSONStream');
+const parseCollection = require('./lib/parse');
+
+let collection = [];
+
+process.stdin.setEncoding('utf8');
+
+process.stdin
+    .pipe(JSONStream.parse())
+    .pipe(es.through( (data) => {
+            collectObjects( data );
+        },
+        () => {
+            const parsedObjects = parseCollection( collection );
+            emitObjects(parsedObjects);
+        }));
+
+function collectObjects(data) {
+    collection.push(data)
+}
+
+function emitObjects(objetos) {
+    let persons_str = '';
+    let orgs_str = '';
+    let members_str = '';
+    let delimiter = '[SPLIT]';
+
+    objetos.persons.map( (person) => { persons_str += JSON.stringify(person) + '\n' } );
+    objetos.orgs.map( (org) => { orgs_str += JSON.stringify(org) + '\n' } );
+    objetos.memberships.map( (member) => { members_str += JSON.stringify(member) + '\n' } );
+
+    process.stdout.write( persons_str + delimiter + orgs_str + delimiter + members_str, process.exit );
+}
