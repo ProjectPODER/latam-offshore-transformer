66f7bc38c19f2cd5901ea00f848a47cb41c05f5f
Fernando Matzdorf <fernando@rindecuentas.org>
2020-09-22 18:04:37 -0500

    Added output of areas for Poppins.

--- index.js
+++ index.js
@@ -24,11 +24,13 @@ function emitObjects(objetos) {
     let persons_str = '';
     let orgs_str = '';
     let members_str = '';
+    let areas_str = '';
     let delimiter = '[SPLIT]';
 
     objetos.persons.map( (person) => { persons_str += JSON.stringify(person) + '\n' } );
     objetos.orgs.map( (org) => { orgs_str += JSON.stringify(org) + '\n' } );
     objetos.memberships.map( (member) => { members_str += JSON.stringify(member) + '\n' } );
+    objetos.areas.map( (area) => { areas_str += JSON.stringify(area) + '\n' } );
 
-    process.stdout.write( persons_str + delimiter + orgs_str + delimiter + members_str, process.exit );
+    process.stdout.write( persons_str + delimiter + orgs_str + delimiter + members_str + delimiter + areas_str, process.exit );
 }
